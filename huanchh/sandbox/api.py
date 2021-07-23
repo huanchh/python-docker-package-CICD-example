d2781a843ae03dd96e334d2d605929305a5a6cea
Victor huang <victorvaphoto@gmail.com>
2021-07-23 16:30:36 -0400

    test

--- huanchh/sandbox/api.py
+++ huanchh/sandbox/api.py
@@ -6,7 +6,7 @@ def get_app() -> FastAPI:
 
     @api.get('/')
     def root():  # pylint: disable=unused-variable
-        return {'hello world'}
+        return {'hello world, hailing from a branch here '}
 
     return api
 
